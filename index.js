76089f0a0444ea63a0a3c7ecabb1d4d77b05cdda
Ariel Campos <ariel.campos94@yahoo.com>
2022-07-28 11:47:18 -0300

    Aula 27: estruturas de repetição com EJS

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ app.get("/:nome/:lang", (req, res) => {
     let nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = true;
+    var produtos = [
+        { nome: "Doritos", preco: 3.14 },
+        { nome: "Coca-cola", preco: 5 },
+        { nome: "Leite", preco: 1.45 }
+    ]
+
     res.render("index", {
         nome,
         lang,
         empresa: "Guia do Programador",
         inscritos: 8210,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos
     });
 });
 
