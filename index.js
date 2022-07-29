d737a7b02639e3bd3f41b056600d73cb9b00b07a
Ariel <ariel.campos94@yahoo.com>
2022-07-28 23:01:45 -0300

    Aula 31: criando o formulário de perguntas

--- index.js
+++ index.js
@@ -8,24 +8,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"))
 
 // Rota
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    var produtos = [
-        { nome: "Doritos", preco: 3.14 },
-        { nome: "Coca-cola", preco: 5 },
-        { nome: "Leite", preco: 1.45 }
-    ]
-
-    res.render("index", {
-        nome,
-        lang,
-        empresa: "Guia do Programador",
-        inscritos: 8210,
-        msg: exibirMsg,
-        produtos
-    });
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
 
 
