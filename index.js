6786e7fbe0c4065a0ce524f37828af26ecf44327
Ariel <ariel.campos94@yahoo.com>
2022-07-29 22:11:21 -0300

    Aula 36: reutilizando código com partials

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"))
 
 // Rota
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 });
